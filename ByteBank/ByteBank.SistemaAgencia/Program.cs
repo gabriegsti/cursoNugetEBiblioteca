fc62ebed4bf6a534575966c77aeb458783533ada
gabregsti <gabregsdevbr@gmail.com>
2022-03-31 10:57:25 -0300

    Usando Nuget com a biblioteca HUmanizer para trabalhar com datas de forma muito tranquila.

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,10 +13,19 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);
 
+            DateTime dataFimPagamento = new DateTime(2022, 5, 25);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40); // dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
         }
+
+        
     }
 }
